31907a7f490444d2fe168edcf5bb2c23b02d52a4
Kevin Helliwell <kbh789@gmail.com>
2022-08-05 10:47:49 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example/user/primeChecker
+
+go 1.19
---
+++ primeChecker.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+import "time"
+
+func isPrime(number int) bool {
+
+	element := 1
+	count := 0
+
+	for element <= number {
+
+		if number%element == 0 {
+			count += 1
+		}
+
+		if count > 2 {
+			return false
+		}
+
+		element += 1
+	}
+
+	return true
+}
+
+func countPrimes(number int) int {
+
+	count := 0
+
+	for index := 1; index <= number; index++ {
+		if isPrime(index) {
+			count += 1
+		}
+	}
+	return count
+}
+
+func main() {
+
+	testNumber := 100_000
+	start := time.Now()
+
+	// for index := 1; index <= testNumber; index++ {
+	// 	fmt.Println(index, isPrime(index))
+	// }
+
+	fmt.Println(countPrimes(testNumber))
+
+	elapsed := time.Since(start)
+	fmt.Println(elapsed)
+}
